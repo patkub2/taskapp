883ffa503b613c68bd47ad9cbb5b2ea874c73aa2
patrykkubala <patrykkubala1999@gmail.com>
2022-09-13 16:58:49 +0200

    mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,65 +1,73 @@
 const { ApolloServer, gql } = require("apollo-server");
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
+const { DB_URI, DB_NAME } = process.env;
 const {
   ApolloServerPluginLandingPageLocalDefault,
 } = require("apollo-server-core");
 
+const typeDefs = gql`
+  type Query {
+    myTaskList: [TaskList!]!
+  }
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    avatar: String
+  }
+  type TaskList {
+    id: ID!
+    createdAt: String!
+    title: String!
+    process: Float!
+    users: [User!]
+    todos: [ToDo!]
+  }
+
+  type ToDo {
+    id: ID!
+    content: String!
+    isCompleted: Boolean!
+
+    TaskList: TaskList!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    myTaskList: () => [],
+  },
+};
+
+const start = async () => {
+  const client = new MongoClient(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const context = {
+    db,
+  };
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    context,
+  });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
+
+start();
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  csrfPrevention: true,
-  cache: "bounded",
-  /**
-   * What's up with this embed: true option?
-   * These are our recommended settings for using AS;
-   * they aren't the defaults in AS3 for backwards-compatibility reasons but
-   * will be the defaults in AS4. For production environments, use
-   * ApolloServerPluginLandingPageProductionDefault instead.
-   **/
-  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-});
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
